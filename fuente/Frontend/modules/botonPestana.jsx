8cb1cebcd676ab2a35673269ec9b450ca073a29d
Juan Meza <diegopaz04717@gmail.com>
2026-04-09 10:45:50 -0500

    Se agrega funcionalidad de volver al menu cuando se presiona cancelar

--- fuente/Frontend/modules/botonPestana.jsx
+++ fuente/Frontend/modules/botonPestana.jsx
@@ -1,14 +1,16 @@
+import { useNavigate } from 'react-router-dom'
 import Boton from "./boton";
 import './styles/botonPestana.css';
 
 function BotonPestana({ opcion, imagen, color, backgroundColor, onClick }) {
+    const Navigate = useNavigate()
     return (
         <div className="botonPestana">
             <Boton color={color} backgroundColor={backgroundColor} onClick={onClick}>
                 <img className="imagen" src={imagen} alt={opcion} />
                 {opcion}
             </Boton>
-            <Boton>
+            <Boton onClick={()=>Navigate('/Menu')}>
                 Cancelar
             </Boton>
         </div>
