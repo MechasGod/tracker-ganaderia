11bd1f6f37a60ce619efed036e6c6cf6bb882ed9
BastianRuiz <juanmunozr@unicauca.edu.co>
2026-05-07 10:24:27 -0500

    cambio de produccion de leche minima

--- fuente/Frontend/src/formValidation.js
+++ fuente/Frontend/src/formValidation.js
@@ -2,7 +2,7 @@ export const VALIDATION_RANGES = {
     PESO_KG: { min: 30, max: 1500 },
     ALTURA_CM: { min: 50, max: 250 },
     CONDICION_CORPORAL: { min: 1, max: 5 },
-    PRODUCCION_LECHE_L_DIA: { min: 0, max: 100 },
+    PRODUCCION_LECHE_L_DIA: { min: 1, max: 100 },
     PRECIO_LECHE_POR_LITRO: { min: 100, max: 100000 },
     COSTO_TOTAL_PERIODO: { min: 0, max: 1000000000 },
     CANTIDAD_ALIMENTO_KG_DIA: { min: 0.1, max: 100 },
